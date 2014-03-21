18d855d3581bab234a76200c30c9c19797a93327
Volker Rose <volker.rose@gmail.com>
2014-03-21 09:43:27 +0100

    changed module format to use amd format only when requirejs/define given

--- src/loadPageModule/loadPageModule.js
+++ src/loadPageModule/loadPageModule.js
@@ -1,33 +1,33 @@
-/*global define, require*/
+(function (define, require) {
+	"use strict";
 
-define(
-	function () {
-		"use strict";
+	/**
+	 * loads and initializes a page specific module via RequireJS
+	 * @param {string} pageId
+	 * @returns {boolean}
+	 * @example
+	 * loadPageModule('index') // loads 'modules/page-index.js' and calls the 'init' method (if given)
+	 */
+	var loadPageModule = function (pageId) {
+		if (typeof pageId === 'undefined' || typeof require === 'undefined') {
+			return false;
+		}
 
-		/**
-		 * loads and initializes a page specific module via RequireJS
-		 * @param {string} pageId
-		 * @returns {boolean}
-		 * @example
-		 * loadPageModule('index') // loads 'modules/page-index.js' and calls the 'init' method (if given)
-		 */
-		var loadPageModule = function (pageId) {
-			if (typeof pageId === 'undefined') {
-				return false;
+		return require(['modules/page-' + pageId], function (page) {
+			if (typeof page !== 'undefined' && typeof page.init === 'function') {
+				return page.init();
 			}
 
-			return require(['modules/page-' + pageId], function (page) {
-				if (typeof page !== 'undefined' && typeof page.init === 'function') {
-					return page.init();
-				}
+			return false;
 
-			}, function (err) {
-				var errorMsg = 'error loading page module "' + err.requireModules[0] + '"';
-				throw new Error(errorMsg);
-			});
+		}, function (err) {
+			var errorMsg = 'error loading page module "' + err.requireModules[0] + '"';
+			throw new Error(errorMsg);
+		});
+	};
 
-		};
-
-		return loadPageModule;
+	if (typeof define === "function") {
+		define( "loadPageModule", [], function () { return loadPageModule; } );
 	}
-);
+
+}(define, require));
